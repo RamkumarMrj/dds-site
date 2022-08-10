b2d9ea26b1a88f311a10cf9ed7a21089efae74ad
RamkumarMrj <ramkumar.mrj97@gmail.com>
2022-08-10 17:49:29 +0530

    first commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -41,29 +41,47 @@ class CenteredText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
+      // alignment: Alignment.center,
       child: Center(
         child: Column(
           children: <Widget>[
-            const SelectableText(
-              "Good things take time..🤍 Digital Document Scanner",
+            const Text("\n"),
+            const Text(
+              "🤍 Digital Document Scanner",
               style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.w200),
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w300),
               textScaleFactor: 4,
             ),
+            const Divider(),
+            const Text("\n"),
+            const SelectableText(
+              "Digital Document Scanner allows the user to scan and save documents either in local storage or in cloud. By using this type of scanner, nobody can face the issue while they are switching between various platforms like Android, IOS or any web application.",
+              style: TextStyle(
+                  color: Color(0xFFE8E3E3), fontWeight: FontWeight.w100),
+              textScaleFactor: 4,
+            ),
+            const Text("\n"),
             Center(
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[
+                children: <Widget>[
                   ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(200, 50),
+                      maximumSize: const Size(200, 50),
+                    ),
                     onPressed: _launchUrl,
-                    child: Text("Visit Web App"),
+                    child: const Text("Visit Web App"),
                   ),
-                  Text("\t\t\t"),
+                  const Text("\t\t\t"),
                   ElevatedButton(
-                    onPressed: _launchUrl,
-                    child: Text("Download App"),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(200, 50),
+                      maximumSize: const Size(200, 50),
+                    ),
+                    onPressed: _launchUrl2,
+                    child: const Text("Download App"),
                   ),
                 ],
               ),
@@ -77,6 +95,15 @@ class CenteredText extends StatelessWidget {
 
 _launchUrl() async {
   const url = 'https://digi.scanall.app';
+  if (await canLaunchUrlString(url)) {
+    await launchUrlString(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchUrl2() async {
+  const url = 'https://github.com/RamkumarMrj/dds-site/releases';
   if (await canLaunchUrlString(url)) {
     await launchUrlString(url);
   } else {
